4613fe9e8f99dca443fd25b284df97d811231214
esalx <51724651+esalx@users.noreply.github.com>
2021-03-19 15:19:37 -0500

    Update __init__.py

--- molli/__init__.py
+++ molli/__init__.py
@@ -1,23 +1,11 @@
-<<<<<<< Updated upstream
-__version__ = "0.1.1"
-
-import molli._config
-import molli.dtypes
-import molli.parsing
-import molli.drivers
-=======
 # import molli._config
 # import molli.dtypes
 # import molli.parsing
 # import molli.drivers
 
+__version__ = "0.1.1"
+
 from . import _config
 from . import dtypes
 from . import drivers
 from . import parsing
-
-# For convenience, main objects and functions should be imported as well
-from 
-
-__version__ = "0.1.1"
->>>>>>> Stashed changes
