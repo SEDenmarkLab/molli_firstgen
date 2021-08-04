e5e3c30b3f07b26d25abc372c9db6301db0675bd
Ian <nir2@illinois.edu>
2021-08-04 18:28:44 -0500

    Revert "Fixed repo for NWChem driver"
    This reverts commit e4eee2c590b82558916a11267451d1e01823bf25.

--- _logging.py
+++
@@ -1,9 +0,0 @@
-# This file is a part of cheminfolib
-
-"""
-    This module sets up the logging functionality.
-    It is extremely useful in analysis of the module actions. 
-"""
-import logging as lg
-
-# TODO do something here
--- dtypes/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-    All data type objects are contained in this module
-"""
-from .geometry import CartesianGeometry
-from .molecule import Atom, Bond, Molecule
-from .collection import Collection, CollectionFile
